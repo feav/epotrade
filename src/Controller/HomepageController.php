36e0e1f463cae4e266b0c89fa5f6fceae73902e5
alex <azebaze@ausiteodit.com>
2020-04-21 15:43:51 +0200

    debut wizard inscription

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageController extends AbstractController
+{ 
+	public function index(Request $request)
+    {
+		return $this->render('website/index.html.twig', []);
+	}
+	public function inscription(Request $request)
+    {
+		return $this->render('website/inscription.html.twig', []);
+	}
+}
